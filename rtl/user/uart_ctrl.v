6e721535a2e8c03714b0ddbdaed27f62bfdf213e
y232578y <139454474+y232578y@users.noreply.github.com>
2024-01-14 02:04:36 +0800

    Update uart_ctrl.v

--- rtl/user/uart_ctrl.v
+++ rtl/user/uart_ctrl.v
@@ -9,10 +9,11 @@ module ctrl(
     output reg        o_wb_ack,
     output reg [31:0] o_wb_dat,
     input wire [7:0]  i_rx,
-    input wire        i_irq,
+    input wire        irq,
     input wire        i_rx_busy,
     input wire        i_frame_err,
-    output reg        o_rx_finish,
+    output reg        o_irq,
+    output reg        o_rx_notfull,
     output reg [7:0]  o_tx,
     input wire        i_tx_start_clear,
     input wire        i_tx_busy,
@@ -37,7 +38,6 @@ localparam STAT_REG = 32'h3000_0008;
 //|        |    31-6     |  5          |  4            |  3        |  2         |  1        |  0        |
 //+------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
 
-reg [31:0] rx_buffer;
 reg [31:0] tx_buffer;
 reg [31:0] stat_reg;    
 reg tx_start_local;
@@ -58,7 +58,7 @@ always@(posedge clk or negedge rst_n)begin
 
         if(i_frame_err && i_rx_busy)
             stat_reg[5] <= 1'b1;
-        else if(i_irq && !stat_reg[1] && !i_frame_err)
+        else if(irq && !stat_reg[1] && !i_frame_err)
             stat_reg[1:0] <= 2'b10;
         else if(i_rx_busy && stat_reg[1:0]==2'b10)
             stat_reg[4] <= 1'b1;
@@ -66,7 +66,11 @@ always@(posedge clk or negedge rst_n)begin
             stat_reg[1:0] <= 2'b01;
     end
 end
+//------------------------------------------
+//-----------------TX-----------------------
+//------------------------------------------
 
+reg [7:0] tx_fifo;
 
 always@(posedge clk or negedge rst_n)begin
     if(!rst_n || i_tx_start_clear)begin
@@ -80,26 +84,113 @@ always@(posedge clk or negedge rst_n)begin
     end
 end
 
+always@(posedge clk or negedge rst_n)begin
+    if(!rst_n || i_tx_start_clear)begin
+        o_tx <= 8'b0;
+        o_tx_start <= 1'b0;
+    end else begin
+        o_tx <= tx_buffer[7:0];
+        o_tx_start <= tx_start_local;
+    end
+end
+
+//------------------------------------------
+//-----------------RX-----------------------
+//------------------------------------------
 
 always@(posedge clk or negedge rst_n)begin
     if(!rst_n)begin
-        rx_buffer <= 32'h00000000;
+        o_rx_notfull <= 1'b0;
     end else begin
-        if(i_irq && !stat_reg[1] && !i_frame_err)begin 
-            rx_buffer <= i_rx;
-            //$display("rx_buffer: %d", i_rx);
-        end
+        if(!rx_fifo_full || i_frame_err)
+            o_rx_notfull <= 1'b1;
+        else 
+            o_rx_notfull <= 1'b0;
     end
 end
 
 always@(posedge clk or negedge rst_n)begin
     if(!rst_n)begin
+        o_wb_ack <= 1'b0;
+    end else begin
+        case(i_wb_adr)
+        RX_DATA:begin
+            if(rx_fifo_rd_en_buf)  
+            o_wb_ack <= 1'b1;
+            else            
+            o_wb_ack <= 1'b0;
+        end
+        
+        default:begin 
+            if(i_wb_valid)  
+            o_wb_ack <= 1'b1;
+            else            
+            o_wb_ack <= 1'b0;
+        end
+        endcase
+    end
+end
+
+wire rx_fifo_full;
+wire rx_fifo_empty;
+wire [7:0] rx_fifo;
+wire rx_fifo_wr_en;
+wire rx_fifo_rd_en;
+
+// assign rx_fifo_wr_en =  irq && !stat_reg[1] && !i_frame_err;
+assign rx_fifo_wr_en =  irq && !rx_fifo_full;
+
+wire rx_fifo_rd_flag;
+assign rx_fifo_rd_flag = (i_wb_valid && !i_wb_we && i_wb_adr==RX_DATA) ? 1 : 0;
+
+reg rx_fifo_rd_en_buf;
+
+always@(posedge clk)begin
+    if(!rst_n)begin
+        rx_fifo_rd_en_buf <= 1'b0;
+    end 
+    else rx_fifo_rd_en_buf<= rx_fifo_rd_flag;
+end
+
+assign rx_fifo_rd_en =  rx_fifo_rd_flag & ~rx_fifo_rd_en_buf;
+
+
+sync_FIFO fifo_rx(
+        .clk(clk), 
+        .reset_n(rst_n),
+        .wr_en(rx_fifo_wr_en), 
+        .rd_en(rx_fifo_rd_en),
+        .data_in(i_rx),
+        .data_out(rx_fifo),
+        .fifo_full(rx_fifo_full), 
+        .fifo_empty(rx_fifo_empty)
+        );
+
+// wire tx_fifo_full;
+// wire tx_fifo_empty;
+// wire tx_fifo_wr_en;
+// wire tx_fifo_rd_en;
+
+// sync_FIFO fifo_tx(
+//         .clk(clk), 
+//         .reset_n(rst_n),
+//         .wr_en(tx_fifo_wr_en), 
+//         .rd_en(tx_fifo_rd_en),
+//         .data_in(tx_fifo),
+//         .data_out(rx_fifo),
+//         .fifo_full(rx_fifo_full), 
+//         .fifo_empty(rx_fifo_empty)
+//         );
+
+always@(posedge clk or negedge rst_n)begin
+    if(!rst_n)begin
         o_wb_dat <= 32'h00000000;
     end else begin
-        if(i_wb_valid && !i_wb_we)begin
+        // if(i_wb_valid && !i_wb_we)begin
+            if(i_wb_valid && !i_wb_we)begin
             case(i_wb_adr)
                 RX_DATA:begin
-                    o_wb_dat <= rx_buffer;
+                    o_wb_dat <= rx_fifo;
                 end
                 STAT_REG:begin
                     o_wb_dat <= stat_reg;
@@ -111,37 +202,4 @@ always@(posedge clk or negedge rst_n)begin
         end
     end
 end
-
-always@(posedge clk or negedge rst_n)begin
-    if(!rst_n)begin
-        o_rx_finish <= 1'b0;
-    end else begin
-        if((i_wb_valid && i_wb_adr==RX_DATA && !i_wb_we && stat_reg[1:0]==2'b10) || i_frame_err)
-            o_rx_finish <= 1'b1;
-        else 
-            o_rx_finish <= 1'b0;
-    end
-end
-
-always@(posedge clk or negedge rst_n)begin
-    if(!rst_n || i_tx_start_clear)begin
-        o_tx <= 8'b0;
-        o_tx_start <= 1'b0;
-    end else begin
-        o_tx <= tx_buffer[7:0];
-        o_tx_start <= tx_start_local;
-    end
-end
-
-always@(posedge clk or negedge rst_n)begin
-    if(!rst_n)begin
-        o_wb_ack <= 1'b0;
-    end else begin
-        if(i_wb_valid)  
-            o_wb_ack <= 1'b1;
-        else            
-            o_wb_ack <= 1'b0;
-    end
-end
-
 endmodule
